0c4612cc31bc687dd9714d5509c083cad5fb7857
blobaugh <ben@lobaugh.net>
2011-09-22 23:53:58 -0500

    Filled in code stubs providing functionality to read and write ini files

--- Ini.class.php
+++ Ini.class.php
@@ -1,3 +1,4 @@
+<?php
 /*
  * This will eventually become a full class for managing ini files from
  * PHP. Currently the PHP language only supports reading ini files, this
@@ -8,22 +9,87 @@
 
 class Ini {
 
+        /**
+         * Holds an associative array of the ini file
+         * @var Array
+         */
+         private $mIni;
+         
+         
 	// Reads in an ini file and places the contents in 
 	// a class level assoc array
-	function open($file){}
+	public function open($file){
+            $this->mIni = parse_ini_file($file, true);
+            
+            print_r($this->mIni);
+        }
 
-	// Sets a value in the ini file
-	// Should this write out to the file immediately?
-	function set($section, $key, $value){}
+	/**
+         * Sets a new value in the ini file
+         * 
+         * If a key needs to be in a section set the name of the section and 
+         * then the key as parameters
+         * 
+         * If no section is needed set the section as the key and $key as 
+         * the value
+         * 
+         * @param String $section
+         * @param String $key
+         * @param String $value 
+         */
+	public function set($section, $key, $value = null){
+            if(isset($value)) {
+                $this->mIni[$section][$key] = $value;
+            } else {
+                $this->mIni[$section][$key];
+            }
+        }
 
-	// Returns the value of the specified section or key
-	function get($section, $key){}
+	/**
+         * Returns the value or array of values from the ini file
+         * 
+         * @param String $section
+         * @param String $key
+         * @return String|Array 
+         */
+	public function get($section, $key = null){
+            if(is_null($key) && isset($this->mIni[$section])) {
+                return $this->mIni[$section];
+            } else if(isset($this->mIni[$section][$key])) {
+                return $this->mIni[$section][$key];
+            } 
+            return null;
+        }
 
 	// Writes the values to the ini file
-	function save($filename){}
+	public function save($file){
+            file_put_contents($file, $this->buildIniFileString());
+        }
+        
+        /**
+         * Builds the string that will be added to the ini file
+         */
+        private function buildIniFileString() {
+            $s = "";
+            foreach($this->mIni as $sec => $key) {
+                if(is_array($key)) {
+                    $s .= "\n[$sec]";
+                    foreach($key as $k => $v) {
+                        $s .= "\n$k = $v";
+                    }
+                } else {
+                    $s .= "\n$sec = $key";
+                }
+            }
+            return $s;
+        }
 
 
-	// Prints out the contents of the object
-	function __toString(){}
+	/**
+         * Prints the contents of the ini file
+         */
+	function __toString(){
+            print_r($this->mIni);
+        }
 
 } // end class
